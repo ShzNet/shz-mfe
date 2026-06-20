cc38453de0c40dce616c4c279674b84cd9643136
Chien Tran <chientranit@gmail.com>
2026-06-20 21:52:38 +0700

    remove sample test

--- sample/host-ws/apps/sample-host/src/app/types.ts
+++
@@ -1,9 +0,0 @@
-import type { HostRemoteApp } from '../remotes'
-
-export type AuthState = {
-  email: string
-}
-
-export type ThemeMode = 'light' | 'dark' | 'system'
-
-export type AppDefinition = HostRemoteApp

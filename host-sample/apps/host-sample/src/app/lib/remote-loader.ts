8798aa59778bcfea337cf6c2d8d1291134022600
Chien Tran <chientranit@gmail.com>
2026-06-20 11:13:42 +0700

    remove sample

--- host-sample/apps/host-sample/src/app/lib/remote-loader.ts
+++
@@ -1,5 +0,0 @@
-import { loadFederatedModule } from '@shz/core'
-
-export async function loadRemoteModule<TModule = unknown>(remoteName: string, exposedModule: string, entry: string): Promise<TModule> {
-  return loadFederatedModule<TModule>(remoteName, exposedModule, entry)
-}
